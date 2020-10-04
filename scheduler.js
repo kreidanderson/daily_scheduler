a584a4538ecfcf8273ea355a431cb2cc72b5883c
Katie R-A <kreidanderson@msn.com>
2020-10-03 21:17:21 -0500

    I was able to figure out how to use moment.js and add the live date to the top of the page.

--- scheduler.js
+++ scheduler.js
@@ -1,30 +1,19 @@
-$("#empty-div").html("<h1>Hello friends!</h1>");
+// var today = $("#currentDay");
+// var saveBtn = $(".saveBtn");
+// var userInput =$(".input");
+// var m = moment()
 
-// jQuery alternative to: var newDiv = document.createElement("div");
-var newDiv = $("<div>");
+// console.log(m)
 
-// jQuery alternative to: newDiv.textContent = "A pleasure to meet you!";
-newDiv.text("A pleasure to meet you!");
+// m.
+// toString()
 
-// jQuery alternative to: document.querySelector("#empty-div").appendChild(newDiv);
-$("#empty-div").append(newDiv);
+// userInput.on("keydown")
+// saveBtn.on("click", function(){
 
-// If we need to apply some CSS, we can quickly do so, using the jQuery ".attr" method.
-newDiv.attr("class", "fancy");
-//above is all for reference -- comment or delete when testing the javascript.
-_________________________________________________________________
+// });
 
-// How can I get the third child of every row to be the save button.
-// figure that out.
-$(".row")
+// if hour < time 
+// .removeAttr("class", "")
 
-//then get the second child of every row to be the event input
-
-
-var saveBtn = $(".saveBtn");
-
-//if hour has passed, you should remove the present and future classes
-//if the hour is present, you should remove the future and past classes
-//if the hour is in the future you should remove the past and present classes
-
-//how do we get a function that will realize what time it is?
+// if 
