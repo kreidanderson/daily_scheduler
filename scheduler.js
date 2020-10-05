6dc1ee1384121ab74e039f24736e8e60c0bc05de
Katie R-A <kreidanderson@msn.com>
2020-10-04 22:06:42 -0500

    Still no buttons working but almost everything else is done

--- scheduler.js
+++ scheduler.js
@@ -4,6 +4,16 @@
 
 
 
+    $('.saveBtn').on('click',function(){
+
+    });
+
+
+// var input = $("input")
+// input.$('keyup', (e) =>{
+//     localStorage.attr(input);
+// })
+
 var today = $("#currentDay");
 var ts = Date.now()
 var m = moment(ts)
@@ -18,11 +28,14 @@ currentHour = m.format('HH')
 //      var time = $(this).parent.attr("id")
 //      localStorage.setItem(time, value);
 //  })
- $(".saveBtn").on("click", function(){
-    var value = $("textarea").val();
-    //  var time = $(this).attr("id")
-     localStorage.setItem(time, value);
- })
+// // textContent;
+//  $(".saveBtn").on("click", function(event){
+//     event.preventDefault();
+//     var value = $("textarea").val();
+//      var time = $(this).attr("id")
+//      window.localStorage.attr("value", time);
+//      console.log(window.localStorage.setItem(value))
+//  })
 
 
 
@@ -35,11 +48,7 @@ currentHour = m.format('HH')
 // console.log(userInput.attr("id"))
 var timeId = $("input")
 
-
-// var changeInputRed = function(){
-//   $("#8").attr("class", "present");
-//   }
-
+// If you look below, we are taking the current time and if it is equal to the time in the scheduler, it will go red (for present time)
 if (currentHour == "08"){
   $("#8").attr("class", "col col-md-8 col-2 past present")
 }
@@ -70,6 +79,40 @@ else if (currentHour == "16"){
 else if (currentHour == "17"){
   $("#17").attr("class", "col col-md-8 col-2 past present")
 }
+
+//In this part the scheduler will turn green if something is in the future.
+if (currentHour < "08"){
+    $("#8").attr("class", "col col-md-8 col-2 past future")
+  }
+  else if (currentHour < "09"){
+    $("#9").attr("class", "col col-md-8 col-2 past future")
+  }
+  else if (currentHour < "10"){
+    $("#10").attr("class", "col col-md-8 col-2 past future")
+  }
+  else if (currentHour < "11"){
+    $("#11").attr("class", "col col-md-8 col-2 past future")
+  }
+  else if (currentHour < "12"){
+    $("#12").attr("class", "col col-md-8 col-2 past future")
+  }
+  else if (currentHour < "13"){
+    $("#13").attr("class", "col col-md-8 col-2 past future")
+  }
+  else if (currentHour < "14"){
+    $("#14").attr("class", "col col-md-8 col-2 past future")
+  }
+  else if (currentHour < "15"){
+    $("#15").attr("class", "col col-md-8 col-2 past future")
+  }
+  else if (currentHour < "16"){
+    $("#16").attr("class", "col col-md-8 col-2 past future")
+  }
+  else if (currentHour < "17"){
+    $("#17").attr("class", "col col-md-8 col-2 past future")
+  }
+
+
   // var times = $("#8","#9","#10","#11","#12","#13","#14","#15","#16","#17");
  
  //it needs to change the specific hour ONLY to red and for some reason when you do this it deletes existing class tags.
@@ -77,10 +120,6 @@ else if (currentHour == "17"){
 // if(currentHour < timeId){
 //   userInput.Attr("class", "future");
 // }
-
-// // var times = $("#8","#9","#10","#11","#12","#13","#14","#15","#16","#17");
-// console.log(JSON.stringify(times))
-// JSON.stringify(times)
 
 
 
@@ -91,19 +130,4 @@ else if (currentHour == "17"){
 // })
 
 
-// $("input").click(function () {
-//   alert(this.attr('value'));
-// });
-// these should be turning gray (at night) because it already happened. 
 
-//when figuring out if the time is the same as each hour you stringify the time and then take the first number from each time and array and see if they match up 
-
-
-
-
-
-
-// $('keydown', function(event) {
-//   $('input').innerHTML.text
-
-// });
